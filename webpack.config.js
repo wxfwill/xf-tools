b6d3f1818a140327087478e3564f09db5e1032e5
尉新飞 <18810563686@163.com>
2021-09-26 18:26:42 +0800

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack/webpack.base.config");
 console.log(`webpack.${process.env.XF_ENV}.config`);
-
 // const devConfig = require("./webpack/webpack.dev.config");
 // const prodConfig = require("./webpack/webpack.prod.config");
 // let config = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
